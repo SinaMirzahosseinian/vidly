d7e8ef3a4dfa2b9ad8fde99e349fc979322bfada
sinamirzahosseinian <sina.mirza2@gmail.com>
2018-09-25 19:22:21 +0330

    pagination completed

---
+++ src/components/utils/paginate.jsx
@@ -0,0 +1,10 @@
+import _ from "lodash";
+
+//paginate data in clinet side
+export function paginate(items, pageNuamber, pageSize) {
+  const startIndex = (pageNuamber - 1) * pageSize;
+  return _(items)
+    .slice(startIndex)
+    .take(pageSize)
+    .value();
+}
